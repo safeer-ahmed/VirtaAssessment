47ca0a9737df0110a9fe675166c3fca2bd7d4148
Safeer <safeerahmedqureshi@gmail.com>
2022-09-16 00:53:26 +0300

    Added put API method

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -6,13 +6,19 @@ const getEndpoint = (endPoint: string) => {
   return 'https://apitest.virta.fi/v4/' + endPoint;
 };
 
-export const commonAPICall = async (requestedURL: string, payload: any) => {
+export const commonAPICall = async (
+  requestedURL: string,
+  payload: any,
+  put: boolean = false,
+) => {
   try {
     let data;
 
     const url = getEndpoint(requestedURL);
 
-    if (payload) {
+    if (put) {
+      data = await (await axios.put(url, payload)).data;
+    } else if (payload) {
       data = await (await axios.post(url, payload)).data;
     } else {
       data = await (await axios.get(url)).data;
